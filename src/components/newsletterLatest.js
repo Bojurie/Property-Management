4cc4ea49891f8bca5d3bbbf4096760127e576300
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-15 15:11:47 -0700

    hooked up latestnewsletter and datebox component to store to fetch data

--- src/components/newsletterLatest.js
+++ src/components/newsletterLatest.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import Button from './button';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import Button from "./button";
 
 class NewsletterLatest extends Component {
+  handleEdit = () => {
+    this.props.history.push("/newsletter/edit");
+  };
 
-    handleEdit = () => {
-       this.props.history.push('/newsletter/edit');
-    }
-
-    render() {
-        const { title, imageUrl, body } = this.props;
-        return (
-            <div className='newsletter-latest'>
-                <h1 className='newsletter-latest__title'>{title}</h1>
-                <img className='newsletter-latest__image' src={imageUrl}/>
-                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon='fas fa-pencil-alt'/>
-                <div className='newsletter-latest__body'>
-                    <p>{body}</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { title, imageUrl, body } = this.props;
+    return (
+      <div className="newsletter-latest">
+        <h1 className="newsletter-latest__title">{title}</h1>
+        <img className="newsletter-latest__image" src={imageUrl} />
+        <Button
+          className="newsletter-latest__button"
+          callback={() => this.handleEdit()}
+          icon="fas fa-pencil-alt"
+        />
+        <div className="newsletter-latest__body">
+          <p>{body}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsletterLatest;
+function mapStateToProps(state) {
+  const { newsletters } = state.newsletters;
+  const latestNewsletter = newsletters[0];
+  return {
+    ...latestNewsletter
+  };
+}
+
+export default connect(mapStateToProps)(NewsletterLatest);
