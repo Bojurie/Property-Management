4929f273497da89a7d231028182a92bf91642f67
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-18 13:40:21 -0700

    built newsletter detail route and component and setup to newsletter id via req.params

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -30,6 +30,7 @@ import Signin from "./components/auth/signin";
 import Dashboard from "./components/dashboard";
 import NewNewsletter from "./components/newsletter/newsletterNew";
 import EditNewsletter from "./components/newsletter/newsletterEdit";
+import NewsletterDetail from "./components/newsletter/newsletterDetail";
 
 function main() {
   ReactDOM.render(
@@ -49,6 +50,10 @@ function main() {
             <Route
               path="/newsletter/edit/:id"
               component={requireAuth(EditNewsletter)}
+            />
+            <Route
+              path="/newsletter/detail/:id"
+              component={requireAuth(NewsletterDetail)}
             />
           </Layout>
         </Switch>
