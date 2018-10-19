689a3d40a1b4e1650b331a5b57549e1838220373
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-19 09:19:18 -0700

    started building requests feature

---
+++ src/components/request/requestGrid.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class RequestGrid extends Component {
+  render() {
+    return <div className="requests-grid">Hi there</div>;
+  }
+}
+export default RequestGrid;
