4f023f16db65650aa5b08c8f8e6a99b33f9de21b
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-19 08:14:02 -0700

    cleaned up newsletter detail grid

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -6,6 +6,8 @@ import * as actions from "../../actions";
 import NewsletterBox from "./newsletterBox";
 import NewsletterLatest from "../newsletterLatest";
 
+import { FormTitle } from "../formTitle";
+
 class NewsletterDetail extends Component {
   componentDidMount() {
     this.props.fetchNewsletterWithId(this.props.match.params.id);
@@ -15,6 +17,10 @@ class NewsletterDetail extends Component {
     console.log(this.props.newsletterToEdit);
     return (
       <div className="newsletter-detail">
+        <FormTitle
+          className="newsletter-detail__title"
+          text="Newsletter detail"
+        />
         <NewsletterBox date={this.props.newsletterToEdit.date} />
         <NewsletterLatest {...this.props.newsletterToEdit} />
       </div>
