8b01b9e419412d362d7357c8476197e9a594c3da
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-20 00:01:53 -0700

    built requests item component base and applied a grid to requests component

--- src/components/request/requests.js
+++ src/components/request/requests.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import RequestsItem from "./requestsItem";
 
 class Requests extends Component {
   render() {
-    return <div className="requests">item 1 item 2 item 3 item 4</div>;
+    return (
+      <div className="requests">
+        <RequestsItem />
+        <RequestsItem />
+        <RequestsItem />
+      </div>
+    );
   }
 }
 export default Requests;
---
+++ src/components/request/requestsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RequestsItem extends Component {
+  render() {
+    return <div className="requests-item">items goes here</div>;
+  }
+}
+
+export default RequestsItem;
