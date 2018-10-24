762c35d42b595155e6456991593430f22124fe3f
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-24 14:36:03 -0700

    added cases for icons for each request type in the requests.js component

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -37,14 +37,23 @@ class RequestsItem extends Component {
   }
 
   render() {
-    const {title, body, date, imageUrl} = this.props;
-
+    const {title, body, date, imageUrl, status} = this.props;
     const parsedDate = new Date(date);
+
+    var moveButtonIcon = 'fas fa-wrench';
+    var mainIcon = "fas fa-exclamation-triangle"
+    if(status =='in-progress') {
+      moveButtonIcon = 'fas fa-check-square'
+      mainIcon = 'fas fa-wrench'
+    }else if (status == 'complete') {
+      moveButtonIcon ='fas fa-exclamation-triangle'
+      mainIcon = 'fas fa-check-square'
+    } 
     return (
       <div id="requests-item" className="requests-item">
         <Icon
           className="requests-item__icon"
-          icon="fas fa-exclamation-triangle"
+          icon={mainIcon}
         />
         <div className="requests-item__title">
           <div className="requests-item__title__text">{title}</div>
@@ -61,7 +70,7 @@ class RequestsItem extends Component {
         </div> 
         <Button
           className="requests-item__move"
-          icon="fas fa-wrench"
+          icon={moveButtonIcon}
           callback={() => this.handleStatus()}
         />
         <div className="requests-item__description">
