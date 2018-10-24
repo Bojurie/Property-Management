5f24dbf661cfba2495eddcfcf9137af9e47bafdd
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-24 14:03:50 -0700

    implemented callback into changeStatus to re-fetch items upon change status completion

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -49,7 +49,7 @@ export function fetchRequests() {
   };
 }
 
-export function changeStatus({ _id, status }) {
+export function changeStatus({ _id, status }, success) {
   const token = localStorage.getItem("token");
   return function() {
     axios
@@ -62,6 +62,7 @@ export function changeStatus({ _id, status }) {
       )
       .then(response => {
         console.log(response.data);
+        success();
       })
       .catch(err => {
         console.log(err);
