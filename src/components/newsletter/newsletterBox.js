5ae392e3cbc649b83693368a43c362ac99e3db1f
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-25 13:08:15 -0700

    parsed date objects in newsletter components to remove date errors

--- src/components/newsletter/newsletterBox.js
+++ src/components/newsletter/newsletterBox.js
@@ -1,36 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
 class NewsletterBox extends Component {
-  render() {
-    const { date } = this.props;
-    if (!date) {
-      return <div>...fetching newsletters</div>;
+    render() {
+        const { date } = this.props;
+        if(!date) {
+            return <div>fetching...</div>
+        }
+        const parsedDate = new Date(date);
+        return (
+            <div className='newsletter-box'>
+                <div className='newsletter-box__day'>{parsedDate.getDate()}</div>
+                <div className='newsletter-box__month-year'>{months[parsedDate.getMonth()]} {parsedDate.getFullYear()}</div>
+                <div className='newsletter-box__point'></div>
+            </div>
+        )
     }
-    return (
-      <div className="newsletter-box">
-        <div className="newsletter-box__day">{date.getDate()}</div>
-        <div className="newsletter-box__month-year">
-          {months[date.getMonth()]} {date.getFullYear()}
-        </div>
-        <div className="newsletter-box__point" />
-      </div>
-    );
-  }
 }
 
 export default NewsletterBox;
--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
-import {connect } from 'react-redux';
+
+import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 import NewNewsletterForm from "./newsletterNewForm";
 
 class NewNewsletter extends Component {
+
   onSubmit = fields => {
 
     const { title, body, image } = fields;
 
     var formData = new FormData();
-    formData.append("title", title);
-    formData.append("body", body);
-    formData.append("image", image);
+    formData.append('title', title);
+    formData.append('body', body);
+    formData.append('image', image);
 
     this.props.createNewNewsletter(formData, () => {
-      this.props.history.push("/dashboard");
-    });
-
+        this.props.history.push("/dashboard");
+    })
+    
   };
 
   onCancel = () => {
@@ -30,16 +32,17 @@ class NewNewsletter extends Component {
         <NewNewsletterForm
           onCancel={() => this.onCancel()}
           onSubmit={event => this.onSubmit(event)}
-          formTitle="New Newsletter"
-          fieldOnePlaceholder="Newsletter Title"
-          fieldOneTitle="Newsletter Title"
-          fieldTwoPlaceholder="Body Here"
-          fieldTwoTitle="Body"
+          formTitle='New Newsletter'
+          fieldOnePlaceholder='Newsletter Title'
+          fieldOneTitle='Newsletter Title'
+          fieldTwoPlaceholder='Body Here'
+          fieldTwoTitle='Body'
         />
       </div>
     );
   }
 }
-NewNewsletter = connect (null, actions)(NewNewsletter);
+
+NewNewsletter = connect(null, actions)(NewNewsletter);
 
 export default NewNewsletter;
