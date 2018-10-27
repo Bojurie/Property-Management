b94f3a044619bd7211e2801753d39aee234ac656
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-26 17:45:21 -0700

    set up admin account

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,6 +5,14 @@ import { ROOT_URL } from "../config";
 import { AUTHENTICATE_USER } from "./types";
 
 export function signUp(fields, success) {
+  // var newFields ={
+  //   admin: true
+  // }
+  // newFields = {
+  //   ...fields,
+  //   ...newFields
+  // }
+
   return function(dispatch) {
     axios
       .post(`${ROOT_URL}/signUp`, fields)
